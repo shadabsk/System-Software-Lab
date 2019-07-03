d4763852c4b4eee0b4d709eb0139670670a41596
shadabsk <shaikhshadabali2@gmail.com>
2019-07-03 17:26:05 +0530

    implemented finding of identifier

---
+++ Experiment 4 - Identifier/Program.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int auto(){
+	int a;
+	int shadab;
+	int 1;
+	void _12;
+	float @spcc;
+	char spcc;
+	int main;
+}
+
+float auto1(){
+	float some;
+	char 125;
+
+}
+double return(){
+
+}
+
+struct defines(){
+
+}
+
+void s1(){
+
+}
